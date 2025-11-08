531164daaca7f92db92f1ea866c5d69209953c0a
Rafael Gioffi <rafaelgioffi@gmail.com>
2025-11-08 00:07:36 -0300

    aula 23

---
+++ Class/dist/tuplas.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let coordenada;
+coordenada = [10, 20];
+console.log(coordenada[0]);
+console.log(coordenada[1]);
+console.log(coordenada[2]);
+let [x, y] = coordenada;
+console.log(x, y);
---
+++ Class/src/tuplas.ts
@@ -0,0 +1,11 @@
+let coordenada: [number, number];
+
+coordenada = [10, 20];
+
+console.log(coordenada[0]);
+console.log(coordenada[1]);
+console.log(coordenada[2]);
+
+let [x, y] = coordenada;
+
+console.log(x, y);
